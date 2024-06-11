c20b842c58fdd555e535612ee1b04ea119111b8d
Roberto0611 <ochoacuevasr@gmail.com>
2024-06-11 13:40:33 -0700

    delete unnecessary js and move html javascript into a single file

--- barCode.js
+++ barCode.js
@@ -1,34 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
-        // Inicializar Quagga
-        Quagga.init({
-            inputStream: {
-                name: "Live",
-                type: "LiveStream",
-                target: document.querySelector('#interactive'), // El contenedor para la cámara
-                constraints: {
-                    facingMode: "environment" // Usar la cámara trasera si está disponible
-                }
-            },
-            decoder: {
-                readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader"]
-            }
-        }, function (err) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log("Initialization finished. Ready to start");
-            Quagga.start();
-        });
+const quaggaConf = {
+    inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector("#camera"),
+        constraints: {
+            width: { min: 640 },
+            height: { min: 480 },
+            facingMode: "environment",
+            aspectRatio: { min: 1, max: 2 }
+        }
+    },
+    decoder: {
+        readers: ['code_128_reader']
+    },
+};
 
-        Quagga.onDetected(function (data) {
-            const code = data.codeResult.code;
-            document.getElementById('result').innerText = `Código detectado: ${code}`;
-            // Detener la captura de video una vez que se haya detectado un código
-            Quagga.stop();
-        });
-    } else {
-        console.log("getUserMedia not supported");
-    }
+const startCamera = () => {
+    document.getElementById("camera").style.display = "block";
+    Quagga.init(quaggaConf, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        Quagga.start();
+    });
+};
+
+document.getElementById("start-camera").addEventListener("click", startCamera);
+
+Quagga.onDetected(function (result) {
+    const code = result.codeResult.code;
+    document.getElementById('barcode').value = code;
+    Quagga.stop();
+    document.getElementById("camera").style.display = "none";
 });
